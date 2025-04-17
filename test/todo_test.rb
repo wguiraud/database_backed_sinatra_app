a28a8d727d10ed2ef893ff1c012be71e4a7817a5
William <wguiraud@protonmail.com>
2025-04-17 15:12:07 -0700

    extracting session manipulation code which helps identify/build the core application API/functionalities

--- test/todo_test.rb
+++ test/todo_test.rb
@@ -13,7 +13,7 @@ class TodoTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_loading_home_page
+  def test_load_home_page
     get "/"
     assert_equal 302, last_response.status
 
@@ -21,7 +21,20 @@ class TodoTest < Minitest::Test
     assert_equal 200, last_response.status
   end
 
-  def test_viewing_all_lists
+  def test_creating_list_with_invalid_names
+    post "/lists", { list_name: "" }
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "List name must be between 1 and 100 characters."
+
+    post "/lists", { list_name: "groceries" }
+    post "/lists", { list_name: "groceries" }
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "List name must be unique."
+  end
+
+  def test_viewing_all_lists #get "/lists"
     #test focusing on the front end implementation
     post "/lists", { list_name: "groceries" }
     assert_equal 302, last_response.status
@@ -31,7 +44,7 @@ class TodoTest < Minitest::Test
     assert_includes last_response.body, "<h2>groceries</h2>"
   end
 
-  def test_session_lists_structure
+  def test_session_lists_structure #get "/lists"
     #test focusing on the back end implementation
     post "/lists", { list_name: "groceries" }
 
@@ -41,11 +54,105 @@ class TodoTest < Minitest::Test
     assert_equal "groceries", lists.first[:name].downcase
   end
 
-  def test_load_list
+  def test_creating_a_new_valid_list
+    post "/lists", { list_name: "groceries" }
 
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
 
+    assert_includes last_response.body, "The list has been created."
+    assert_includes last_response.body, "groceries"
+  end
 
+  def test_removing_a_list
+    post "/lists", { list_name: "groceries" }
 
+    post "/lists/1/destroy"
+    assert_equal 302, last_response.status
+
+    get last_response['location']
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "The list has been deleted."
+  end
+
+  def test_updating_a_list
+    post "/lists", { list_name: "groceries" }
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The list has been created."
+    assert_includes last_response.body, "groceries"
+
+    post "/lists/1", { list_name: "wines"}
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The list has been updated."
+    assert_includes last_response.body, "wines"
+  end
+
+  def test_adding_a_todo_to_a_list
+    post "/lists", { list_name: "groceries" }
+
+    post "/lists/1/todos", { todo: "red wine"}
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The todo was added."
+    assert_includes last_response.body, "red wine"
+  end
+
+  def test_removing_todo_from_list
+    post "/lists", { list_name: "groceries" }
+    post "/lists/1/todos", { todo: "red wine"}
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The todo was added."
+    assert_includes last_response.body, "red wine"
+
+    post "/lists/1/todos/1/destroy"
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The todo has been deleted."
+  end
+
+  def test_updating_todo_status
+    post "/lists", { list_name: "groceries" }
+    post "/lists/1/todos", { todo: "red wine"}
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The todo was added."
+    assert_includes last_response.body, "red wine"
+
+    post "/lists/1/todos/1", { completed: true }
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "The todo has been updated"
+  end
+
+  def test_mark_list_todos_as_completed
+    post "/lists", { list_name: "groceries" }
+    post "/lists/1/todos", { todo: "red wine"}
+    post "/lists/1/todos", { todo: "white wine"}
+
+    post "lists/1/complete_all"
+    assert_equal 302, last_response.status
+    get last_response['location']
+    assert_equal 200, last_response.status
+
+    assert_includes last_response.body, "All todos have been completed."
   end
 
 end
--- todo.rb
+++ todo.rb
@@ -2,6 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "erubi"
+require "pry"
+
+require_relative 'session_persistence'
 
 configure do
   require_relative 'config/environments/test' if test?
@@ -42,17 +45,6 @@ helpers do
   end
 end
 
-class SessionPersistance
-
-  def initialize(session)
-    @session = session
-  end
-
-  def find_list(id)
-    @session[:lists].find{ |list| list[:id] == id }
-  end
-end
-
 def load_list(id)
   list = @storage.find_list(id)
   return list if list
@@ -66,7 +58,7 @@ end
 def error_for_list_name(name)
   if !(1..100).cover? name.size
     "List name must be between 1 and 100 characters."
-  elsif session[:lists].any? { |list| list[:name] == name }
+  elsif @storage.all_lists.any? { |list| list[:name] == name }
     "List name must be unique."
   end
 end
@@ -78,14 +70,8 @@ def error_for_todo(name)
   end
 end
 
-def next_element_id(elements)
-  max = elements.map { |todo| todo[:id] }.max || 0
-  max + 1
-end
-
-
 before do
-  @storage = SessionPersistance.new(session)
+  @storage = SessionPersistence.new(session)
   session[:lists] ||= []
 end
 
@@ -95,7 +81,7 @@ end
 
 # View list of lists
 get "/lists" do
-  @lists = session[:lists]
+  @lists = @storage.all_lists
   erb :lists, layout: :layout
 end
 
@@ -103,8 +89,8 @@ end
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
-
 # Create a new list
+
 post "/lists" do
   list_name = params[:list_name].strip
 
@@ -113,8 +99,8 @@ post "/lists" do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    id = next_element_id(session[:lists])
-    session[:lists] << { id: id, name: list_name, todos: [] }
+    @storage.add_new_list(list_name)
+
     session[:success] = "The list has been created."
     redirect "/lists"
   end
@@ -145,7 +131,7 @@ post "/lists/:id" do
     session[:error] = error
     erb :edit_list, layout: :layout
   else
-    @list[:name] = list_name
+    @storage.update_list_name(id, list_name)
     session[:success] = "The list has been updated."
     redirect "/lists/#{id}"
   end
@@ -154,7 +140,8 @@ end
 # Delete a todo list
 post "/lists/:id/destroy" do
   id = params[:id].to_i
-  session[:lists].reject! { |list| list[:id] == id }
+  @storage.remove_list(id)
+
   session[:success] = "The list has been deleted."
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
@@ -174,8 +161,7 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    id = next_element_id(@list[:todos])
-    @list[:todos] << { id: id, name: text, completed: false }
+    @storage.add_new_todo(@list_id, text)
 
     session[:success] = "The todo was added."
     redirect "/lists/#{@list_id}"
@@ -188,7 +174,8 @@ post "/lists/:list_id/todos/:id/destroy" do
   @list = load_list(@list_id)
 
   todo_id = params[:id].to_i
-  @list[:todos].reject! { |todo| todo[:id] == todo_id }
+  @storage.remove_todo(@list_id, todo_id)
+
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
   else
@@ -204,8 +191,7 @@ post "/lists/:list_id/todos/:id" do
 
   todo_id = params[:id].to_i
   is_completed = params[:completed] == "true"
-  todo = @list[:todos].find { |todo| todo[:id] == todo_id }
-  todo[:completed] = is_completed
+  @storage.update_todo(@list_id, todo_id, is_completed)
 
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
@@ -216,9 +202,7 @@ post "/lists/:id/complete_all" do
   @list_id = params[:id].to_i
   @list = load_list(@list_id)
 
-  @list[:todos].each do |todo|
-    todo[:completed] = true
-  end
+  @storage.complete_all_todos(@list_id)
 
   session[:success] = "All todos have been completed."
   redirect "/lists/#{@list_id}"
